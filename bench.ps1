95088664d9966ea8fec267fb0418280a00c01f13
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-21 01:02:03 +0200

    `dev` --> `main` (#3)
    * build: update dependencies
    
    * chore: remove unneeded types
    
    * chore: remove Cargo.lock from gitignore
    
    * build: update dependencies
    
    * build: update ctrlc & add thiserror
    
    * build: add rustfmt max_width
    
    * chore: add Hyperfine benchmark script
    
    * chore: delete unused file
    
    * fix: bench command
    
    * build: update dependencies
    
    * ref: use `Library` instead of `Wad` as name
    
    * ref: use new names
    
    * perf: remove unnecessary data clone
    
    * perf: improve performance from 2.096s to 667.7ms 🔥🔥🔥
    
    * feat: re-enable ctrl-c
    
    * ref: remove ctrl-c

---
+++ bench.ps1
@@ -0,0 +1,1 @@
+hyperfine --prepare 'if exist "E:\Phill030.de\Rust\KiWad-Unpacker\target\release\output" rmdir /s /q "E:\Phill030.de\Rust\KiWad-Unpacker\target\release\output"' 'E:\Phill030.de\Rust\KiWad-Unpacker\target\release\kiwad_unpacker.exe CSR.wad'
