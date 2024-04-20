78effecb23defcc50d75c33f90ba37c999471c6d
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-20 23:35:47 +0200

    fix: bench command

--- bench.ps1
+++ bench.ps1
@@ -1,1 +1,1 @@
-hyperfine --prepare 'if exist "D:\Phill030.de\Rust\KiWad-Unpacker\target\release\output" rmdir /s /q "D:\Phill030.de\Rust\KiWad-Unpacker\target\release\output"' 'D:\Phill030.de\Rust\KiWad-Unpacker\target\release\kiwad_unpacker.exe C:\Users\pmsal\Desktop\CSR.wad'
+hyperfine --prepare 'if exist "E:\Phill030.de\Rust\KiWad-Unpacker\target\release\output" rmdir /s /q "E:\Phill030.de\Rust\KiWad-Unpacker\target\release\output"' 'E:\Phill030.de\Rust\KiWad-Unpacker\target\release\kiwad_unpacker.exe CSR.wad'
