c9bf7fc5c412bad11cc3354cb84b09c124eb4a72
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-20 23:36:43 +0200

    ref: use `Library` instead of `Wad` as name

--- src/library.rs
+++ src/library.rs
@@ -1,15 +1,14 @@
 use binary_modifier::{BinaryError, BinaryReader, Endian};
-use flate2::DecompressError;
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use std::{
-    collections::HashMap,
+    collections::VecDeque,
     fs::{self, create_dir_all},
     io::{Cursor, Read},
     path::Path,
 };
 
 #[derive(Debug, Clone)]
-pub struct FileRecord {
+pub struct LibraryRecord {
     pub offset: u32,
     pub size: u32,
     pub zip_size: u32,
@@ -19,14 +18,14 @@ pub struct FileRecord {
 }
 
 #[derive(Debug)]
-pub struct WadRework<'a> {
+pub struct Library<'a> {
     pub version: u32,
     pub file_count: u32,
-    pub files: HashMap<String, FileRecord>,
+    pub files: VecDeque<LibraryRecord>,
     pub buffer: &'a mut [u8],
 }
 
-impl<'a> WadRework<'a> {
+impl<'a> Library<'a> {
     /// # Panics
     ///
     /// Will panic if the file doesn't contain a `KIWAD` header!
@@ -46,7 +45,7 @@ impl<'a> WadRework<'a> {
                 reader.read_bytes(1)?;
             }
 
-            let mut files: HashMap<String, FileRecord> = HashMap::new();
+            let mut files: VecDeque<LibraryRecord> = VecDeque::new();
 
             for _ in 0..file_count {
                 let offset = reader.read_u32()?;
@@ -57,25 +56,19 @@ impl<'a> WadRework<'a> {
                 let file_name = reader.read_big_string()?.to_string().replace('\x00', "");
 
                 // Some of them are falsely marked as zipped but aren't actually
-                if file_name.ends_with(".wav")
-                    || file_name.ends_with(".ogg")
-                    || file_name.ends_with(".mp3")
-                {
+                if file_name.ends_with(".wav") || file_name.ends_with(".ogg") || file_name.ends_with(".mp3") {
                     zip = false;
                 }
 
                 // Add the FileRecord to the HashMap with properties
-                files.insert(
-                    file_name.clone(),
-                    FileRecord {
-                        zip_size,
-                        crc32: crc,
-                        file_name,
-                        zipped: zip,
-                        offset,
-                        size,
-                    },
-                );
+                files.push_back(LibraryRecord {
+                    zip_size,
+                    crc32: crc,
+                    file_name,
+                    zipped: zip,
+                    offset,
+                    size,
+                });
             }
 
             Ok(Self {
@@ -90,7 +83,7 @@ impl<'a> WadRework<'a> {
     }
 
     pub fn open_all_files(&mut self, path: &mut Path) {
-        self.files.par_iter().for_each(|(file_name, file_record)| {
+        self.files.par_iter().for_each(|file_record| {
             let mut buffer = Vec::with_capacity(file_record.size as usize);
             let mut cursor = Cursor::new(&self.buffer);
 
@@ -122,59 +115,12 @@ impl<'a> WadRework<'a> {
                 buffer = data.clone();
             }
 
-            //? TODO: Maybe add tokio::spawn and make all this async ??
-
-            let path = &mut path.join(file_name);
+            let path = &mut path.join(&file_record.file_name);
             create_dir_all(path.parent().unwrap()).unwrap();
             fs::write(&path, &buffer).unwrap_or_else(|e| eprintln!("Could not write to file! {e}")); // Write to the file
 
             buffer.clear();
         });
-    }
-
-    /// # Panics
-    ///
-    /// Will panic if the name does not exist in the `HashMap`
-    pub fn read_file(&mut self, name: &str) -> Result<Vec<u8>, DecompressError> {
-        let file_record = self
-            .files
-            .get(name)
-            .expect("File does not exist inside of wad!");
-
-        let mut buffer: Vec<u8> = Vec::with_capacity(file_record.size as usize);
-        let mut cursor: Cursor<&&mut [u8]> = Cursor::new(&self.buffer);
-
-        let data = {
-            let mut result = vec![
-                0;
-                if file_record.zipped {
-                    file_record.zip_size
-                } else {
-                    file_record.size
-                } as usize
-            ];
-
-            cursor.set_position(u64::from(file_record.offset));
-            cursor.read_exact(&mut result).unwrap();
-            result
-        };
-
-        if file_record.zipped {
-            if Self::is_empty(&data) {
-                buffer.clear();
-            } else {
-                let mut decompressor = flate2::Decompress::new(true);
-                decompressor.decompress_vec(
-                    &data[..],
-                    &mut buffer,
-                    flate2::FlushDecompress::Finish,
-                )?;
-            }
-        } else {
-            buffer = data.clone();
-        }
-
-        Ok(buffer)
     }
 
     /// Check if a `Vec<u8>` is the magic header `KIWAD`
